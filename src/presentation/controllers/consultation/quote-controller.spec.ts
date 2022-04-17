6a7bf10a7ab72da4a38a593cb4b87e90f751c9b8
TonySandro <tonysduarte101@gmail.com>
2022-04-17 20:52:56 -0300

    test: ensure QuoteController returns 200 if returns correct values

--- src/presentation/controllers/consultation/quote-controller.spec.ts
+++ src/presentation/controllers/consultation/quote-controller.spec.ts
@@ -1,5 +1,15 @@
 import { MissingParamError } from "../../errors"
+import { HttpResponse } from "../../protocols"
 import { QuoteController } from "./quote-controller"
+
+const makeFakeResponse = (): HttpResponse => ({
+    statusCode: 200,
+    data: {
+        name: "IBM",
+        lastPrice: "126.7000",
+        pricedAt: "2022-04-14"
+    }
+})
 
 interface SutTypes {
     sut: QuoteController
@@ -18,6 +28,15 @@ describe('Quote Controller', () => {
         const httpResponse = await sut.handle({ data: { quoteName: "IBM" } })
 
         expect(httpResponse.statusCode).toBe(200)
+    })
+
+    test('Should return 200 if returns correct values', async () => {
+        const { sut } = makeSut()
+        jest.spyOn(sut, "handle").mockReturnValueOnce(
+            new Promise(resolve => resolve(makeFakeResponse())))
+
+        const httpResponse = await sut.handle({ data: { quoteName: "IBM" } })
+        expect(httpResponse).toEqual(makeFakeResponse())
     })
 
     test('Should returns api with valid data', async () => {
