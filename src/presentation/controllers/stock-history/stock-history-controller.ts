321224742e3ccfc1379eee1abb5e8b4ee1b2a1ea
TonySandro <tonysduarte101@gmail.com>
2022-04-18 21:54:38 -0300

    feat: ensure StockHistoryController return 400 if no from is provided

--- src/presentation/controllers/stock-history/stock-history-controller.ts
+++ src/presentation/controllers/stock-history/stock-history-controller.ts
@@ -1,4 +1,5 @@
-import { serverError, success } from "../../helpers/http/http-helper"
+import { MissingParamError } from "../../errors"
+import { badRequest, serverError, success } from "../../helpers/http/http-helper"
 import { Controller, HttpRequest, HttpResponse } from "../../protocols"
 
 export class StockHistoryController implements Controller {
@@ -6,8 +7,43 @@ export class StockHistoryController implements Controller {
 
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
         try {
+            const requiredFields = ['from', 'to']
 
-            return success(null)
+            for (const field of requiredFields) {
+                if (!httpRequest.data[field]) {
+                    return badRequest(new MissingParamError(field))
+                }
+            }
+
+            return success({
+                name: 'any',
+                prices: [
+                    {
+                        opening: 14.05,
+                        low: 13.77,
+                        high: 14.6,
+                        closing: 14.35,
+                        pricedAt: '2021-10-22',
+                        volume: 36461100,
+                    },
+                    {
+                        opening: 14.49,
+                        low: 13.77,
+                        high: 14.61,
+                        closing: 14.16,
+                        pricedAt: '2021-10-21',
+                        volume: 34002600,
+                    },
+                    {
+                        opening: 15.68,
+                        low: 14.85,
+                        high: 15.68,
+                        closing: 15.01,
+                        pricedAt: '2021-10-20',
+                        volume: 36340900,
+                    },
+                ],
+            })
         } catch (error) {
             return serverError(error)
         }
