15495cd5191c192d5bbe750e08c2c5a4097d4803
TonySandro <tonysduarte101@gmail.com>
2022-04-18 22:02:40 -0300

    test: ensure StockHistoryController return 400 if no from is provided

--- src/presentation/controllers/stock-history/stock-history-controller.spec.ts
+++ src/presentation/controllers/stock-history/stock-history-controller.spec.ts
@@ -1,8 +1,41 @@
+import { MissingParamError } from "../../errors"
 import { HttpRequest, HttpResponse } from "../../protocols"
 import { StockHistoryController } from "./stock-history-controller"
 
+const makeFakeResponse = (): HttpResponse => ({
+    statusCode: 200,
+    data: {
+        name: 'any',
+        prices: [
+            {
+                opening: 14.05,
+                low: 13.77,
+                high: 14.6,
+                closing: 14.35,
+                pricedAt: '2021-10-22',
+                volume: 36461100,
+            },
+            {
+                opening: 14.49,
+                low: 13.77,
+                high: 14.61,
+                closing: 14.16,
+                pricedAt: '2021-10-21',
+                volume: 34002600,
+            },
+            {
+                opening: 15.68,
+                low: 14.85,
+                high: 15.68,
+                closing: 15.01,
+                pricedAt: '2021-10-20',
+                volume: 36340900,
+            },
+        ],
+    }
+})
 
-const makeFakeRequest = (): HttpRequest => ({ data: { quoteName: "IBM" } })
+const makeFakeRequest = (): HttpRequest => ({ data: { from: "10", to: "12" } })
 
 interface SutTypes {
     sut: StockHistoryController
@@ -21,5 +54,30 @@ describe('Stock History Controller', () => {
         const httpResponse = await sut.handle(makeFakeRequest())
 
         expect(httpResponse.statusCode).toBe(200)
+    })
+
+    test('Should return 200 if returns correct values', async () => {
+        const { sut } = makeSut()
+        jest.spyOn(sut, "handle").mockReturnValueOnce(
+            new Promise(resolve => resolve(makeFakeResponse()))
+        )
+
+        const httpResponse = await sut.handle(makeFakeRequest())
+        expect(httpResponse).toEqual(makeFakeResponse())
+    })
+
+    test('Should return 400 if no from is provided', async () => {
+        const { sut } = makeSut()
+
+        const httpRequest = {
+            data: {
+                // from: "10",
+                to: "12"
+            }
+        }
+
+        const httpResponse = await sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.data.message).toEqual(new MissingParamError('from'))
     })
 })
