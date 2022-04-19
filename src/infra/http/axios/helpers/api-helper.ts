0058473dccde8adda668d7f57102ac1ac8c8568b
TonySandro <tonysduarte101@gmail.com>
2022-04-19 19:13:41 -0300

    feat: add fetchStockHistory to a quote month

--- src/infra/http/axios/helpers/api-helper.ts
+++ src/infra/http/axios/helpers/api-helper.ts
@@ -1,14 +1,27 @@
 import { api } from "../api/api"
 
 const key = "JVIEUBBVF4KCEK99"
-const timeSeries = "TIME_SERIES_INTRADAY"
+const funcTimeSeries = "TIME_SERIES_INTRADAY"
+const funcMonthly = "function=TIME_SERIES_MONTHLY_ADJUSTED"
 
-export const fetchQuote = async (quote: string) => {
-    const quoteData = await api.get(
-        `query?function=${timeSeries}&symbol=${quote}&interval=5min&apikey=${key}`
-    ).then(res => {
-        return res.data
-    })
+export const ApiHelper = {
+    async fetchQuote(quote: string) {
+        const quoteData = await api.get(
+            `query?function=${funcTimeSeries}&symbol=${quote}&interval=5min&apikey=${key}`
+        ).then(res => {
+            return res.data
+        })
 
-    return quoteData["Time Series (5min)"]
+        return quoteData["Time Series (5min)"]
+    },
+
+    async fetchStockHistory(quote: string) {
+        const stockHistory = await api.get(
+            `query?${funcMonthly}&symbol=${quote}&apikey=${key}`
+        ).then(res => {
+            return res.data
+        })
+
+        return stockHistory["Monthly Adjusted Time Series"]
+    }
 }
