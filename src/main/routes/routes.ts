c5530401e2dd6ab3601d90a091a926b2ca5fce07
TonySandro <tonysduarte101@gmail.com>
2022-04-19 21:21:23 -0300

    feat: add StockHistory route

--- src/main/routes/routes.ts
+++ src/main/routes/routes.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { makeQuoteController } from "../factories/quote/quote-factory";
-import { adaptRoute } from "../adapters/express/express-routes-adapter";
+import { adaptRoute, adaptRouteStockHistory } from "../adapters/express/express-routes-adapter";
+import { makeStockHistoryController } from "../factories/stock-history/stock-history-factory";
 
 export default (route: Router): void => {
     route.get('/stock/:quoteName/quote', adaptRoute(makeQuoteController()))
+    route.get(`/stocks/:stockName/history?`, adaptRouteStockHistory(makeStockHistoryController()))
 }
