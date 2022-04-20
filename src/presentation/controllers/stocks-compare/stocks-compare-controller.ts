5c63bea366509a228df1a7fd8c7f6b46e6e70651
TonySandro <tonysduarte101@gmail.com>
2022-04-20 17:54:55 -0300

    feat: ensure StocksCompareController return 400 if no quoteName is provided

--- src/presentation/controllers/stocks-compare/stocks-compare-controller.ts
+++ src/presentation/controllers/stocks-compare/stocks-compare-controller.ts
@@ -1,8 +1,22 @@
-import { Controller, HttpRequest, HttpResponse } from "@presentation/protocols";
+import { MissingParamError } from "../../errors";
+import { badRequest } from "../../helpers/http/http-helper";
+import { Controller, HttpRequest, HttpResponse } from "../../protocols";
 
 export class StocksCompareController implements Controller {
     constructor() { }
-    handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-        return new Promise(resolve => resolve({ statusCode: 200, data: {} }))
+    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+        const { stocksToCompare, quoteName } = httpRequest.data
+
+        const requiredFields = ['quoteName', 'stocksToCompare']
+        for (const field of requiredFields) {
+            if (!httpRequest.data[field]) {
+                return badRequest(new MissingParamError(field))
+            }
+        }
+
+        return {
+            statusCode: 200,
+            data: {}
+        }
     }
 }
--- src/presentation/errors/external-server-error.ts
+++
@@ -1,6 +0,0 @@
-export class ExternalServerError extends Error {
-    constructor() {
-        super(`External server error`)
-        this.name = `External server error`
-    }
-}
