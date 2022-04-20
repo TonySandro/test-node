0b7003edd65de5c94a3a5ac40399e0218928114e
TonySandro <tonysduarte101@gmail.com>
2022-04-19 21:23:46 -0300

    feat: add adapt express route from StockHistory

--- src/main/adapters/express/express-routes-adapter.ts
+++ src/main/adapters/express/express-routes-adapter.ts
@@ -4,10 +4,22 @@ import { Request, Response } from 'express'
 
 export const adaptRoute = (controller: Controller) => {
     return async (req: Request, res: Response) => {
-        const stockName = req.params.stockName
+        const httpRequest: HttpRequest = {
+            data: req.params
+        }
+
+        const httpResponse = await controller.handle(httpRequest)
+        res.status(httpResponse.statusCode).json(httpResponse.data)
+    }
+}
+
+export const adaptRouteStockHistory = (controller: Controller) => {
+    return async (req: Request, res: Response) => {
         const httpRequest: HttpRequest = {
             data: {
-                quoteName: stockName.toUpperCase()
+                stockName: req.params.stockName,
+                from: req.query.from,
+                to: req.query.to
             }
         }
 
