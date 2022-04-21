e4e4a233f9afa688d307cb14a323eb43afac3ba4
TonySandro <tonysduarte101@gmail.com>
2022-04-21 13:54:09 -0300

    feat: ensure StockGainsController return 500 if lastQuote throws

--- src/presentation/controllers/stock-gains/stock-gains-controller.ts
+++ src/presentation/controllers/stock-gains/stock-gains-controller.ts
@@ -1,17 +1,32 @@
+import { LastQuoteDay } from "../../../data/usecases/filter/last-quote-day";
+import { ApiHelper } from "../../../infra/http/axios/helpers/api-helper";
 import { MissingParamError } from "../../errors";
-import { badRequest } from "../../helpers/http/http-helper";
+import { badRequest, serverError } from "../../helpers/http/http-helper";
 import { success } from "../../helpers/http/http-helper";
 import { Controller, HttpRequest, HttpResponse } from "../../protocols";
 
 export class StockGainsController implements Controller {
-    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-        const requiredField = ["stockName", "purchasedAt", "purchasedAmount"]
+    constructor(private readonly lastQuote: LastQuoteDay) {
+        this.lastQuote = lastQuote
+    }
 
-        for (const field of requiredField) {
-            if (!httpRequest.data[field]) {
-                return badRequest(new MissingParamError(field))
+    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+        try {
+            const { stockName, purchasedAt, purchasedAmount } = httpRequest.data
+            const requiredField = ["stockName", "purchasedAt", "purchasedAmount"]
+
+            for (const field of requiredField) {
+                if (!httpRequest.data[field]) {
+                    return badRequest(new MissingParamError(field))
+                }
             }
+
+            const allQuote = await ApiHelper.fetchQuote(stockName)
+            this.lastQuote.filter(allQuote, stockName)
+
+            return success("")
+        } catch (error) {
+            return serverError(error)
         }
-        return success("")
     }
 }
