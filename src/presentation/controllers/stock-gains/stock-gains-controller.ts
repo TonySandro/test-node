1199c752b4925dc88f09f92ce79376628ea6dd14
TonySandro <tonysduarte101@gmail.com>
2022-04-21 11:22:43 -0300

    feat: ensure StockGainsController return 400 if no stockName is provided

--- src/presentation/controllers/stock-gains/stock-gains-controller.ts
+++ src/presentation/controllers/stock-gains/stock-gains-controller.ts
@@ -1,8 +1,17 @@
+import { MissingParamError } from "../../errors";
+import { badRequest } from "../../helpers/http/http-helper";
 import { success } from "../../helpers/http/http-helper";
 import { Controller, HttpRequest, HttpResponse } from "../../protocols";
 
 export class StockGainsController implements Controller {
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+        const requiredField = ["stockName"]
+
+        for (const field of requiredField) {
+            if (!httpRequest.data[field]) {
+                return badRequest(new MissingParamError(field))
+            }
+        }
         return success("")
     }
 }
