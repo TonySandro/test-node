083c54d811c5ca17e5341713c5b7653e120c3e7d
TonySandro <tonysduarte101@gmail.com>
2022-04-22 00:00:29 -0300

    refactor: correction in api params

--- src/infra/http/axios/helpers/api-helper.ts
+++ src/infra/http/axios/helpers/api-helper.ts
@@ -1,6 +1,7 @@
 import env from "../../../../main/config/env"
 import { api } from "../api/api"
 
+const key = env.apiKey
 const funcTimeSeries = "TIME_SERIES_INTRADAY"
 const funcDaily = "function=TIME_SERIES_DAILY"
 
@@ -38,11 +39,10 @@ const fakeResponseToTests = {
 export const ApiHelper = {
     async fetchQuote(quote: string) {
         const quoteData = await api.get(
-            `query?function=${funcTimeSeries}&symbol=${quote}&interval=5min&${env.apiKey}`
+            `query?function=${funcTimeSeries}&symbol=${quote}&interval=5min&apikey=${key}`
         ).then(res => {
             return res.data
         })
-
         return quoteData["Time Series (5min)"]
 
         return fakeResponseToTests
@@ -50,11 +50,10 @@ export const ApiHelper = {
 
     async fetchStockHistory(quote: string) {
         const stockHistory = await api.get(
-            `query?${funcDaily}&symbol=${quote}&${env.apiKey}`
+            `query?${funcDaily}&symbol=${quote}&apikey=${key}`
         ).then(res => {
             return res.data
         })
-
         return stockHistory["Time Series (Daily)"]
 
         return fakeResponseToTests
